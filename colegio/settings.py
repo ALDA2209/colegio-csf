b1dce1852e78f35a37db33909ec06e98af58a8d0
Aldahir <1176049@senati.pe>
2026-05-12 08:45:15 -0500

    Formulario de contacto funcional con envio de correo

--- colegio/settings.py
+++ colegio/settings.py
@@ -116,3 +116,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+# Email configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'pasivi22@gmail.com'
+EMAIL_HOST_PASSWORD = 'emev hfby sslw otvo'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
--- web/views.py
+++ web/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
 
 def inicio(request):
     return render(request, 'web/index.html')
@@ -28,4 +29,22 @@ def docentes(request):
     return render(request, 'web/docentes.html')
 
 def contacto(request):
-    return render(request, 'web/contacto.html')
+    enviado = False
+    if request.method == "POST":
+        nombre = request.POST.get("nombre", "")
+        apellido = request.POST.get("apellido", "")
+        email = request.POST.get("email", "")
+        telefono = request.POST.get("telefono", "")
+        nombre_nino = request.POST.get("nombre_nino", "")
+        edad = request.POST.get("edad", "")
+        asunto = request.POST.get("asunto", "")
+        mensaje = request.POST.get("mensaje", "")
+        cuerpo = "Nombre: " + nombre + " " + apellido + "\nCorreo: " + email + "\nTelefono: " + telefono + "\nNino: " + nombre_nino + "\nEdad: " + edad + "\nAsunto: " + asunto + "\n\nMensaje:\n" + mensaje
+        send_mail(
+            subject="Nuevo mensaje - " + nombre + " " + apellido,
+            message=cuerpo,
+            from_email="pasivi22@gmail.com",
+            recipient_list=["pasivi22@gmail.com"],
+        )
+        enviado = True
+    return render(request, "web/contacto.html", {"enviado": enviado})
