0fc9b831d67fe5ccc8dc04c82ea3a56adfdc66d0
Aldahir <aldahir@LAPTOP-SDHHEAUG.localdomain>
2026-05-12 16:25:30 -0500

    Cambiar puerto SMTP a 465 SSL para Render

--- colegio/settings.py
+++ colegio/settings.py
@@ -79,8 +79,9 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_PORT = 465
+EMAIL_USE_TLS = False
+EMAIL_USE_SSL = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'pasivi22@gmail.com')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD', 'emev hfby sslw otvo')
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
