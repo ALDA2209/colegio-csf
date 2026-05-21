c3696e084097b0d772eddb41d9e5e499f73f6159
Aldahir <1176049@senati.pe>
2026-05-20 19:08:18 -0500

    Customize admin panel

--- web/admin.py
+++ web/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+admin.site.site_header = "IEI Cuna Jardín Carlos Showing Ferrari"
+admin.site.site_title = "Panel de Administración"
+admin.site.index_title = "Bienvenido al Panel de Administración"
+
 # Register your models here.
